4a7f47d9b1840184a1254e32bc7e0d28842c548b
Adrian Parsons <adrian@adrianparsons.com>
2024-10-02 13:28:49 -0400

    using a different map id and loading a different way works?

--- index.ts
+++ index.ts
@@ -4,11 +4,12 @@ async function initMap(): Promise<void> {
   map = new Map(document.getElementById("map") as HTMLElement, {
     center: { lat: 40.7565749, lng: -73.9797362 },
     zoom: 13,
-    //mapId: "3be746a5b0357cb1",
-    mapId: "6e310d529ec45c8",
+    mapId: "3be746a5b0357cb1",
+    //mapId: "6e310d529ec45c8",
+    //mapId: "23cffe40dd78351",
   });
   let layer = map.getDatasetFeatureLayer("aa920d72-66af-4c1b-975a-20c1d16df3de");
-  layer.style = { strokeColor: "green", strokeWeight: 4 };
+  layer.style = { strokeColor: "green", strokeWeight: 2 };
 }
 
-initMap();
+//initMap();
