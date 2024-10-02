973f26aa4e91312826073c674dc02aa43827ef58
Adrian Parsons <adrian@adrianparsons.com>
2024-10-02 12:10:35 -0400

    basic working ts map

--- index.ts
+++ index.ts
@@ -2,11 +2,9 @@ let map: google.maps.Map;
 async function initMap(): Promise<void> {
   const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
   map = new Map(document.getElementById("map") as HTMLElement, {
-    center: { lat: 40.756, lng: -73.979 },
+    center: { lat: 40.7565749, lng: -73.9797362 },
     zoom: 8,
   });
-  //let layer = map.getDatasetFeatureLayer("aa920d72-66af-4c1b-975a-20c1d16df3de");
-
 }
 
 initMap();
