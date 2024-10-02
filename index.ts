c6b268fae62c808d91160d8335bd6648c3b8769b
Adrian Parsons <adrian@adrianparsons.com>
2024-10-02 12:48:08 -0400

    basic loading of places from dataset...

--- index.ts
+++ index.ts
@@ -3,8 +3,12 @@ async function initMap(): Promise<void> {
   const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
   map = new Map(document.getElementById("map") as HTMLElement, {
     center: { lat: 40.7565749, lng: -73.9797362 },
-    zoom: 8,
+    zoom: 13,
+    //mapId: "a026940006728352e",
+    mapId: "6e310d529ec45c8",
   });
+  let layer = map.getDatasetFeatureLayer("aa920d72-66af-4c1b-975a-20c1d16df3de");
+
 }
 
 initMap();
