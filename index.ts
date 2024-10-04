0d9bbfbb06831ef7e018422df06d28041e86366a
Adrian Parsons <adrian@adrianparsons.com>
2024-10-04 17:01:57 -0400

    basic information window

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 let map: google.maps.Map;
+let infoWindow;
+
 async function initMap(): Promise<void> {
-  const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  const { Map, InfoWindow } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
 
   map = new Map(document.getElementById("map") as HTMLElement, {
     center: { lat: 40.7565749, lng: -73.9797362 },
@@ -9,8 +10,11 @@ async function initMap(): Promise<void> {
     mapId: "3be746a5b0357cb1"
   });
 
+  infoWindow = new InfoWindow({pixelOffset: {height: -37}});
+
   //let layer = map.getDatasetFeatureLayer("aa920d72-66af-4c1b-975a-20c1d16df3de");
   //layer.style = { fillColor: "blue", pointRadius: 4 };
+
 
   await map.data.loadGeoJson("json/less_columns.json", {idPropertyName: "address"}, (feature) => {
     for (let ft in feature) {
@@ -18,9 +22,32 @@ async function initMap(): Promise<void> {
     }
     console.log("fuuutuuuures featuressssss!", feature)
   });
+
+  map.data.addListener('click', (e) => showInfo(e.latLng, e.feature));
+
   //await map.data.addGeoJson(geojson);
 
+  // TODO REMOVE THIS
   globalThis.googlemap = map;
+}
+
+function showInfo(position, feature) {
+  const lotArea = Number(feature.getProperty('lotarea')).toLocaleString();
+
+  const content = `
+    <div style="">
+      <h3 style="margin-top: 0">${feature.getProperty('address')}</h3>
+      <p>Owner: ${feature.getProperty('ownername')}</p>
+      <p>${lotArea} square feet</p>
+      <p>community board ${feature.getProperty('community board')}</p>
+      <p>precinct ${feature.getProperty('policeprct')}</p>
+      <p>council district ${feature.getProperty('council district')}</p>  
+    </div>
+  `;
+  // feature.forEachProperty(console.log)
+
+  infoWindow.setOptions({content, position});
+  infoWindow.open({map, shouldFocus: false});
 }
 
 initMap();
