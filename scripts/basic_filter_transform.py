32e51b5afdedb305c27853217fbd9cc613dfbbb2
Adrian Parsons <adrian@adrianparsons.com>
2024-10-22 16:26:46 -0400

    clean up script a little

--- scripts/basic_filter_transform.py
+++ scripts/basic_filter_transform.py
@@ -3,20 +3,22 @@ import pandas as pd
 import argparse
 
 def clean_data(df):
-    # Derive column 'state' from column: 'address'
-    df.insert(15, 'state', df.apply(lambda row : "", axis=1))
     # Rename column 'xcoord' to 'x'
     df = df.rename(columns={'xcoord': 'x'})
     # Rename column 'ycoord' to 'y'
     df = df.rename(columns={'ycoord': 'y'})
     # Drop rows with missing data in column: 'latitude'
     df = df.dropna(subset=['latitude'])
+
+    # Derive column 'state' from column: 'address'
+    df.insert(15, 'state', df.apply(lambda row : "", axis=1))
     # Replace all instances of "" with "NY" in column: 'state'
     df.loc[df['state'].str.lower() == "".lower(), 'state'] = "NY"
     # Clone column 'state' as 'city'
     df['city'] = df.loc[:, 'state']
     # Replace all instances of "NY" with "New York" in column: 'city'
     df.loc[df['city'].str.lower() == "NY".lower(), 'city'] = "New York"
+
     # Drop duplicate rows in column: 'address'
     df = df.drop_duplicates(subset=['address'])
     # Filter rows with addresses that don't start with a number
@@ -35,5 +37,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
