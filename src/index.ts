ddb3af8481d8e83123a6679eb510cf81e696fb5f
Adrian Parsons <adrian@adrianparsons.com>
2025-09-17 19:16:36 -0400

    point streetview int he right direction

--- src/index.ts
+++ src/index.ts
@@ -78,11 +78,21 @@ function showInfo(position: google.maps.LatLng | undefined, feature: google.maps
 }
 
 function showStreetViewPanorama(position: google.maps.LatLng)  {
+  const pos = position
   streetview.getPanorama({
     location: position,
     sources: [google.maps.StreetViewSource.OUTDOOR]
   }, (panoData: google.maps.StreetViewPanoramaData | null) => {
-    panoData?.location?.pano && panorama.setPano(panoData.location.pano)
+
+    const lat1 = panoData?.location?.latLng?.lat()
+    const lng1 = panoData?.location?.latLng?.lng()
+    const lat2 = position.lat()
+    const lng2 = position.lng()
+
+    const heading = turf.rhumbBearing([lat1, lng1],[lat2,lng2])
+    console.log("heading: ", heading)
+
+    panoData?.location?.pano && panorama.setPano(panoData.location.pano) && panorama.setPov({heading, pitch: 33})
   })
 }
 
