af164f5891724d0a4d58efab3bb31635b425ba15
Adrian Parsons <adrian@adrianparsons.com>
2025-10-14 12:08:31 -0400

    add build scripts that use docker

---
+++ build/geojson_to_pmtile.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+set -e
+
+CMD="tippecanoe -f -zg -o /usr/local/tiles/vacant.pmtiles --projection=EPSG:4326 --extend-zooms-if-still-dropping --drop-densest-as-needed -l vacant /usr/local/tiles/vacant.geojson"
+IMG_TAG="tippe:2"
+
+docker run --rm \
+    --mount type=bind,source="$(pwd)"/tiles,target=/usr/local/tiles/ \
+    $IMG_TAG bash -c "$CMD"
