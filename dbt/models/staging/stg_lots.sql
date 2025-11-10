d99dcdad02c283b187c082adaffce4cb883c155a
Adrian Parsons <adrian@adrianparsons.com>
2025-11-10 15:06:31 -0500

    some cleanup

--- dbt/models/staging/stg_lots.sql
+++ dbt/models/staging/stg_lots.sql
@@ -1,17 +1,5 @@
-
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
 {{ config(materialized='view') }}
-
 
 select *
 from {{ source('nyc-lots', 'pluto') }}
 where address is not null
--- maybe this next line isn't that important, as long as bbl isn't replicated.
--- and regexp_contains(address, r'[0-9].*')
--- dbt/models/staging/stg_vacant.sql
+++ dbt/models/staging/stg_vacant.sql
@@ -12,26 +12,3 @@
 select *
 from {{ ref('stg_lots') }}
 where landuse = '11'
-
-/*
-with first_records as (
-    select address, version
-    from {{ ref('stg_lots') }}
-    where landuse = '11'
-    group by address, version
-)
-*/
-
-/*
-select address, version from first_records
-group by version
-*/
-
-/*
-select vacant.*
-from {{ ref('stg_lots') }} vacant
-inner join first_records f
-    on 
-
-*/
-
