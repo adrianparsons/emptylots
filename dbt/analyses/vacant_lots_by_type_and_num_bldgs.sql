2905ebfb9de02617d23f734a33b5eaa63216aac3
Adrian Parsons <adrian@adrianparsons.com>
2025-11-12 15:09:24 -0500

    base queries off of vacant view

--- dbt/analyses/vacant_lots_by_type_and_num_bldgs.sql
+++ dbt/analyses/vacant_lots_by_type_and_num_bldgs.sql
@@ -1,3 +1,3 @@
 select count(bldgclass), bldgclass, numbldgs
-from {{ ref('stg_lots') }}
-where landuse = '11' group by rollup(bldgclass, numbldgs) order by bldgclass
+from {{ ref('stg_vacant') }}
+group by rollup(bldgclass, numbldgs) order by bldgclass
--- dbt/analyses/vacant_lots_by_version.sql
+++ dbt/analyses/vacant_lots_by_version.sql
@@ -1,3 +1,3 @@
 select count(version), version
-from {{ ref('stg_lots') }}
+from {{ ref('stg_vacant') }}
 where landuse = '11' group by version
