92fffae4ef0d0410ed0cd198cc795ce1f7ae62b8
Adrian Parsons <adrian@adrianparsons.com>
2025-11-14 16:37:44 -0500

    change year calculation to make it easier to calculate

--- dbt/models/staging/stg_vacancy.sql
+++ dbt/models/staging/stg_vacancy.sql
@@ -6,6 +6,6 @@ select bbl,
 cast(bbl as string) as bbl_key,
 range(
   date( concat('20', substring(version, 0, 2), '-1-1') ),
-  date( concat('20', substring(version, 0, 2), '-12-31') )
+  date_add(date( concat('20', substring(version, 0, 2), '-1-1')), interval 1 year)
 ) as vacant_year
 from {{ ref('stg_vacant') }}
