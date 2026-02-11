ce16799659f505a4ab4434f3f8c5df1da0707a11
Adrian Parsons <adrian@adrianparsons.com>
2026-02-11 17:50:04 -0500

    get csvs by wildcard match, not filenames

--- build/combine_csvs.sh
+++ build/combine_csvs.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-csvstack pluto_18v2_1.csv pluto_19v2.csv pluto_20v8.csv pluto_21v3.csv pluto_22v3.csv pluto_23v3_1.csv pluto_24v4_1.csv pluto_25v3.csv > pluto_historical.csv
+find historical -type f -name "*.csv" | xargs csvstack > historical/pluto_historical.csv
