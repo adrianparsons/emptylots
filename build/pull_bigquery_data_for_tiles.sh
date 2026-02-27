17036de08bf82ae1b222646a9040a2e907202dc9
Adrian Parsons <adrian@adrianparsons.com>
2026-02-27 15:37:34 -0500

    envsubst did not work, updating

--- build/pull_bigquery_data_for_tiles.sh
+++ build/pull_bigquery_data_for_tiles.sh
@@ -4,4 +4,4 @@ set -e
 
 QUERY_FILE=$1
 
-envsubst < $QUERY_FILE | bq query --use_legacy_sql=false --max_rows=999999 --format=json
+bq query --use_legacy_sql=false --max_rows=999999 --format=json < $QUERY_FILE
