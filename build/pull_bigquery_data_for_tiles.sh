80f58c543c5c8253b006ad550d8c9008aaecc3b8
Adrian Parsons <adrian@adrianparsons.com>
2026-02-27 16:08:35 -0500

    add logging

--- build/pull_bigquery_data_for_tiles.sh
+++ build/pull_bigquery_data_for_tiles.sh
@@ -4,4 +4,4 @@ set -e
 
 QUERY_FILE=$1
 
-bq query --use_legacy_sql=false --max_rows=999999 --format=json < $QUERY_FILE
+bq query --apilog=stdout --use_legacy_sql=false --max_rows=999999 --format=json < $QUERY_FILE
