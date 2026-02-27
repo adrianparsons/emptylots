05063aa08c75d245364c48d13f5c866eedb94cb4
Adrian Parsons <adrian@adrianparsons.com>
2026-02-27 16:41:46 -0500

    output apilog to err

--- build/pull_bigquery_data_for_tiles.sh
+++ build/pull_bigquery_data_for_tiles.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 QUERY_FILE=$1
 
-bq query --apilog=stdout --use_legacy_sql=false --max_rows=999999 --format=json < $QUERY_FILE
+bq query --apilog=stderr --use_legacy_sql=false --max_rows=999999 --format=json < $QUERY_FILE
