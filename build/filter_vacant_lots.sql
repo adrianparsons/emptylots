0a40653908b8bcd02cd2b28bc27fbcdb255ffc38
Adrian Parsons <adrian@adrianparsons.com>
2026-05-30 18:10:14 -0400

    remove now-unused scripts

--- build/filter_vacant_lots.sql
+++
@@ -1,5 +0,0 @@
--- Used as an argument in an ogr2ogr command
--- Filters bad data and selects vacant properties
-select * from MapPLUTO where LandUse = '11'
-and Address is not null
-and substr(Address, 1, 1) in ('0','1','2','3','4','5','6','7','8','9')
