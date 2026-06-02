da14b909a887f94f9bea6f9e0db70914102c6462
Adrian Parsons <adrian@adrianparsons.com>
2026-06-02 15:51:28 -0400

    don't overload geometry field

--- build/parking_lots_from_bq.sql
+++ build/parking_lots_from_bq.sql
@@ -1,5 +1,5 @@
 select
-    parking.*,
+    parking.* except(geometry),
     st_asgeojson(geometry) as geometry
 from `empty-lots.nyc_pluto_historical.mart_parking_lots` as parking
 where geometry is not null
