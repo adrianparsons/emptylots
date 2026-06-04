86479fcda753dfda27201512fc27fa62ecab9924
Adrian Parsons <adrian@adrianparsons.com>
2026-06-04 16:36:06 -0400

    use bbl instead of BBL

--- dbt/models/staging/stg_geometry.sql
+++ dbt/models/staging/stg_geometry.sql
@@ -1,6 +1,6 @@
 {{ config(materialized='table') }}
 
 select
-    safe_cast(BBL as float64 ) as BBL,
+    safe_cast(BBL as float64 ) as bbl,
     st_geogfromgeojson(geometry, make_valid => true) as geometry,
 from {{ source('raw_pluto', 'mappluto_geometry') }}
