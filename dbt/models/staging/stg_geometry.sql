7f9acb004637563d586f97d34132a1dee1da2105
Adrian Parsons <adrian@adrianparsons.com>
2026-06-04 16:34:22 -0400

    generate geometry from raw text

--- dbt/models/staging/stg_geometry.sql
+++ dbt/models/staging/stg_geometry.sql
@@ -1,2 +1,6 @@
-select *
+{{ config(materialized='table') }}
+
+select
+    safe_cast(BBL as float64 ) as BBL,
+    st_geogfromgeojson(geometry, make_valid => true) as geometry,
 from {{ source('raw_pluto', 'mappluto_geometry') }}
