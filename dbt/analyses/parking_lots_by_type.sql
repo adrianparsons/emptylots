d7822cd82c22dc734aa046e78372b4271cc04e0c
Adrian Parsons <adrian@adrianparsons.com>
2026-06-05 11:13:18 -0400

    remove some dangling analyses queries, add a column to one

--- dbt/analyses/parking_lots_by_type.sql
+++ dbt/analyses/parking_lots_by_type.sql
@@ -1,5 +1,5 @@
 
-select count(bldgclass)
+select count(bldgclass), bldgclass
 from {{ ref('stg_lots') }}
 where landuse = '10'
 group by bldgclass
--- dbt/analyses/parking_lots_by_type_and_num_bldgs.sql
+++
@@ -1,3 +0,0 @@
-select count(bldgclass), bldgclass, numbldgs
-from {{ ref('stg_lots') }}
-where landuse = 10 group by rollup(bldgclass, numbldgs) order by bldgclass
