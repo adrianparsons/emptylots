148889b3c4f498d179fbd6fccb8759dc783e61f2
Adrian Parsons <adrian@adrianparsons.com>
2026-06-10 17:19:12 -0400

    don't materialize geometry, for now

--- dbt/models/staging/stg_geometry.sql
+++ dbt/models/staging/stg_geometry.sql
@@ -1,6 +1,6 @@
-{{ config(materialized='table') }}
-
 select
     safe_cast(BBL as float64 ) as bbl,
-    st_geogfromgeojson(geometry, make_valid => true) as geometry,
+    st_geogfromgeojson
+    st_geo(geometry, make_valid => true) as geometry,
 from {{ source('raw_pluto', 'mappluto_geometry') }}
+
