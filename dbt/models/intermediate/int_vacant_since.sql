31678587c9e6fc8fec03eeaa276b5d62b44a5a03
Adrian Parsons <adrian@adrianparsons.com>
2026-06-10 20:33:09 -0400

    some more cleanup, generate current/max year instead of hardcoding

--- dbt/models/intermediate/int_vacant_since.sql
+++ dbt/models/intermediate/int_vacant_since.sql
@@ -3,23 +3,18 @@
 */
 
 with has_vacancy as (
-    select *
-    from from {{ ref('stg_vacant_range') }}
-    -- We want to get ranges from places that are *currently* vacant
-    where range_end(session_range) = max(range_end(session_range))
-),
-
-vacancy_by_bbl as (
-    select distinct
+    select
         bbl_key,
-        range_start(session_range) as range_start
-    from has_vacancy
-),
+        vacant_year,
+        session_range
+    from {{ ref('stg_vacant_range') }}
+    -- We want to get ranges from places that are *currently* vacant
+    where range_end(session_range) = (
+        select max(range_end(vacant_year)) from {{ ref('stg_vacancy') }}
+    )
+)
 
-select
-    filtered.*,
-    range_start as vacant_since,
-from vacancy_by_bbl
-right join {{ ref('stg_vacant') }} as filtered
-    on bbl_key = cast(filtered.bbl as string)
-where filtered.version = {{ current_pluto_version() }}
+select distinct
+    bbl_key,
+    range_start(session_range) as vacant_since
+from has_vacancy
