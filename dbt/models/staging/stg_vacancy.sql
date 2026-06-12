842ba6133e0b8107fc511f582e0617da53f776c5
holistudio <daniel.bin.lu@gmail.com>
2026-06-12 12:52:25 -0400

    add ownername to vacancy table columns

--- dbt/models/staging/stg_vacancy.sql
+++ dbt/models/staging/stg_vacancy.sql
@@ -4,6 +4,7 @@
 
 select bbl,
 cast(bbl as string) as bbl_key,
+ownername,
 range(
   date( concat('20', substring(version, 0, 2), '-1-1') ),
   date_add(date( concat('20', substring(version, 0, 2), '-1-1')), interval 1 year)
