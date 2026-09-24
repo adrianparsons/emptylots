7664b21b6a9bfd0215667a0ff4835238c7720b7c
holistudio <daniel.bin.lu@gmail.com>
2026-09-24 11:49:51 -0400

    add ownertype to stg_vacancy
    ownertype can help indicate whether the owner is city/state/federal government or major private institution (ownertype != 'P')

--- dbt/models/staging/stg_vacancy.sql
+++ dbt/models/staging/stg_vacancy.sql
@@ -6,6 +6,7 @@ select bbl,
 {{ bbl_key_from_parts('borocode', 'block', 'lot') }} as bbl_key,
 
 ownername,
+ownertype,
 range(
   date( concat('20', substring(version, 0, 2), '-1-1') ),
   date_add(date( concat('20', substring(version, 0, 2), '-1-1')), interval 1 year)
